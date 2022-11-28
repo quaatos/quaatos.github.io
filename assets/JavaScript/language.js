094e4d9ab75c91e4f175e7d8ef9afcad8803784a
quaatos <93578678+quaatos@users.noreply.github.com>
2022-11-28 05:29:52 -0800

    Add files via upload

---
+++ assets/JavaScript/language.js
@@ -0,0 +1,44 @@
+//The use of an api is to much in this case, also, i don't see the point of API's. 
+let skills = document.getElementById('skills');
+let languages = document.getElementById('languagesISpeak');
+let dutch = document.getElementById('dutch');
+let english = document.getElementById('english');
+let norwegian = document.getElementById('norwegian');
+let myLocation = document.getElementById('location');
+let Personal = document.getElementById('Personal');
+let Socials = document.getElementById('Socials');
+const filepath = 
+
+function ToNorsk() {
+    skills.innerHTML = "mine ferdigheter:";
+    languages.innerHTML = "språk jeg snakker:";
+    dutch.innerHTML = "Nederlansk";
+    english.innerHTML = "Engelsk";
+    norwegian.innerHTML = "Norsk (læring)";
+    myLocation.innerHTML = "Nederland";
+    Personal.innerHTML = "personlig";
+    Socials.innerHTML = "sosiale medier";
+}
+
+function ToDutch() {
+    skills.innerHTML = "Mijn vaardigheden:";
+    languages.innerHTML = "Talen die ik spreek:";
+    dutch.innerHTML = "Nederlands";
+    english.innerHTML = "Engels";
+    norwegian.innerHTML = "Noors (Aan het leren)";
+    myLocation.innerHTML = "Nederland";
+    Personal.innerHTML = "Persoonlijk";
+    Socials.innerHTML = "Sociaal";
+}
+
+function ToEnglish() {
+    skills.innerHTML = "My skills:";
+    languages.innerHTML = "Languages i speak:";
+    dutch.innerHTML = "Dutch";
+    english.innerHTML = "English";
+    norwegian.innerHTML = "Norwegian (Learning)";
+    myLocation.innerHTML = "The Netherlands";
+    Personal.innerHTML = "Personal";
+    Socials.innerHTML = "Socials";
+}
+
